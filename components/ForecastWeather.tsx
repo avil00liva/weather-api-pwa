5a943a5eedae43f207da46d67de6f084d12f2dc5
Agustin Oliva <joliva@vates.com>
2023-10-20 17:15:29 -0300

    RootState added on ForecastWeather

--- components/ForecastWeather.tsx
+++ components/ForecastWeather.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import LoadingForecastWeather from './LoadingForecastWeather'
 import { useSelector } from 'react-redux'
 
-const ForecastWeather = (props: Props) => {
-  const store = useSelector(state => state.weather.weather)
+type RootState = {
+    weather: {
+      weather: any;
+      error: any; 
+    };
+
+  };
+
+const ForecastWeather = () => {
+  const store = useSelector((state: RootState) => state.weather.weather)
   const storeKeyLength = Object.keys(store || {}).length
 
 
   return (
     <section className='mt-16 border-t py-2 w-full '>
         <h1 className='text-center mb-6'>Forecast</h1>
-        {storeKeyLength > 0 ? store?.data?.forecast.forecastday[0].hour.map(fc => {
+        {storeKeyLength > 0 ? store?.data?.forecast.forecastday[0].hour.map((fc: any) => {
             return (
                 <div key={fc.time_epoch} className='w-full min-h-[60px] rounded-sm shadow-md p-2 flex items-center justify-between'>
                     <div className='flex flex-col gap-4 justify-between h-full text-xl'>
